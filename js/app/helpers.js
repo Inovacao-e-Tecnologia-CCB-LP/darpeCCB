05cd680ba2ab61d28cc7ee358c149d2d085c6be3
Leonardo Silva <yleo545@hotmail.com>
2026-02-03 23:30:51 -0300

    fix: correcoes no botao relatorio

--- js/app/helpers.js
+++ js/app/helpers.js
@@ -12,3 +12,20 @@ function formatarHorario(horario) {
 function isMobile() {
   return window.innerWidth < 768;
 }
+
+function carregarImagemBase64(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      resolve(canvas.toDataURL("image/png"));
+    };
+    img.onerror = reject;
+    img.src = url;
+  });
+}
---
+++ js/app/services/relatorio.service.js
@@ -0,0 +1,24 @@
+class RelatoriosService {
+  async carregarBase() {
+    const inscritos = await inscricoesService.listar();
+
+    return {
+      inscritos,
+      locais: dataStore.locais || [],
+      programacoes: dataStore.programacao || [],
+    };
+  }
+
+  filtrarProgramacoesComInscritos(localId, inscritosPorProgramacao) {
+    return dataStore.programacao.filter((p) => {
+      if (p.local_id != localId) return false;
+      return (inscritosPorProgramacao[p.id] || []).length > 0;
+    });
+  }
+
+  obterColaboradores(programacaoId, inscritosPorProgramacao) {
+    return inscritosPorProgramacao[programacaoId] || [];
+  }
+}
+
+const relatoriosService = new RelatoriosService();
